4f26189b6a49dc661f3bb987115182e12697bad2
mwilson090 <mwilson316@gatech.edu>
2021-08-09 13:43:12 -0500

    front end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import HomePage from './pages/HomePage';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+import NotFoundPage from './pages/NotFoundPage';
+import NavBar from './NavBar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-          <Route path="/" component={HomePage} exact />
+        <NavBar />
+        <div id="page-body">
+          <Switch>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/articles-list" component={ArticlesListPage} />
+            <Route path="/article/:name" component={ArticlePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </div>
       </div>
     </Router>
   );
