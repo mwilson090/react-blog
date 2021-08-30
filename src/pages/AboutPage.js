94893d259484a64a2b2f91085428a7d66111855c
mwilson090 <mwilson316@gatech.edu>
2021-08-30 19:38:21 -0400

    about tweak

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,7 @@ import Hobbit from '../hobbit.jpg';
 const AboutPage = () => (
     <div className="about">
         <img src={Hobbit} width="700" alt="Discord screenshot of The Hobbit book club meeting" />
-        <h3>We are a monthly book club run by Jennifer Peña and Madeline Wilson. We read a variety of genres of fiction books, connecting friends across the US. Check our Discord for meetings and discussion!</h3>
+        <h3>We are a monthly book club run by Jennifer Peña and Madeline Wilson. We read a variety of genres of fiction books, connecting our book club buds across the US. Check our Discord for meetings and discussion!</h3>
     </div>
 );
 
