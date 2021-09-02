1bb216c44462eda45ccb82970c313479cf9e1ca1
mwilson090 <mwilson316@gatech.edu>
2021-09-01 22:31:53 -0400

    typo fix

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
             <Route component={NotFoundPage} />
           </Switch>
         </div>
-        <h5 className="footer">© 2021 Jennifer Peña annd Madeline Wilson. All rights reserved.</h5>
+        <h5 className="footer">© 2021 Jennifer Peña and Madeline Wilson. All rights reserved.</h5>
       </div>
     </Router>
   );
